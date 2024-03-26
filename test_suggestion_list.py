d0d3c27246518853626af6a398d2902e4da777fd
Danya <dan.shell@bk.ru>
2024-03-26 21:36:57 +0300

    add explicity waits

--- test_suggestion_list.py
+++ test_suggestion_list.py
@@ -2,11 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-import time
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 @pytest.fixture(scope="module")
 def setup():
-    
-    webdriver_path = '/usr/local/bin/chromedriver'  
+
+    webdriver_path = '/usr/local/bin/chromedriver'
 
     # Create a new Chrome service object
     chrome_service = ChromeService(webdriver_path)
@@ -23,6 +27,7 @@ def setup():
 
     print("Close browser")
 
+
 def test_suggestion_list(setup):
     driver = setup
 
@@ -35,17 +40,22 @@ def test_suggestion_list(setup):
     # Type the search query
     search_box.send_keys("Selenium")
 
-    
-    time.sleep(3)
+    # time.sleep(3)
 
-    
-    
-    # Get the text of the first suggestion
-    
-    first_suggestion=driver.find_element(By.CSS_SELECTOR,'.mini-suggest__item.mini-suggest__item_type_fulltext.mini-suggest__item_enriched_yes').text
+    try:
+        # Get the text of the first suggestion
+        first_suggestion = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, '.mini-suggest__item.mini-suggest__item_type_fulltext.mini-suggest__item_enriched_yes')))
+
+        # Get the text content of the first suggestion
+        first_suggestion_text = first_suggestion.text
+
+    except:
+        print("Element with CSS selector .mini-suggest__item.mini-suggest__item_type_fulltext.mini-suggest__item_enriched_yes not found or visible within 10 seconds")
 
     # Define the expected first suggestion
     expected_suggestion = "selenium"
 
     # Check if the first suggestion matches the expected suggestion
-    assert first_suggestion == expected_suggestion, f"First suggestion: {first_suggestion} does not match the expected value {expected_suggestion}"
+    assert first_suggestion_text.lower(
+    ) == expected_suggestion, f"First suggestion: {first_suggestion_text} does not match the expected value {expected_suggestion}"
