7c12dbad409515a4d39c590712df8e8fe4d0eb8b
Danya <dan.shell@bk.ru>
2024-03-26 21:21:29 +0300

    remove wrong comments

--- test_suggestion_list.py
+++ test_suggestion_list.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.by import By
 import time
 @pytest.fixture(scope="module")
 def setup():
-    # Set the path to your webdriver executable
-    webdriver_path = '/usr/local/bin/chromedriver'  # Use the correct path
+    
+    webdriver_path = '/usr/local/bin/chromedriver'  
 
-    # Specify the path to the ChromeDriver executable using the Service class
+    # Create a new Chrome service object
     chrome_service = ChromeService(webdriver_path)
 
     # Create a new instance of the Chrome driver
@@ -35,19 +35,13 @@ def test_suggestion_list(setup):
     # Type the search query
     search_box.send_keys("Selenium")
 
-    # Wait for suggestions to appear
-    # suggestion_list = driver.find_element(By.CLASS_NAME,'mini-suggest__item mini-suggest__item_type_fulltext mini-suggest__item_enriched_yes')
-
-    # document.getElementsByClassName('mini-suggest__item mini-suggest__item_type_fulltext mini-suggest__item_enriched_yes')[0].innerText
-
+    
     time.sleep(3)
 
-    # suggestion_list = driver.find_element(By.CLASS_NAME,'mini-suggest__group')
-    suggestion_list = driver.find_elements(By.CLASS_NAME,'mini-suggest__item mini-suggest__item_type_fulltext mini-suggest__item_enriched_yes')
     
     
     # Get the text of the first suggestion
-    # first_suggestion = suggestion_list[0].text
+    
     first_suggestion=driver.find_element(By.CSS_SELECTOR,'.mini-suggest__item.mini-suggest__item_type_fulltext.mini-suggest__item_enriched_yes').text
 
     # Define the expected first suggestion
